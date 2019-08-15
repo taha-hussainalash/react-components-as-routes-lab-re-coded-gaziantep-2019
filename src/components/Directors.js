c49cd2795b02805e56fa378f78360fc7cb1ffe8d
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-15 10:57:47 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,18 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+     <h1>Directors Page</h1>
+      {directors.map((director) => (
+        <div>
+          <h1>Name: {director.name}</h1>
+          <p>Movies:</p>
+          <ul>
+            {director.movies.map((movie) =>(
+                <li>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
